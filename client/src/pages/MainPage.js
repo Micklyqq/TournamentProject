25938c9a6cc00f8bc170f2f9af89096ccb775b8e
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-26 12:36:43 +0300

    add auth and profile to mainpage

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../css/mainpage.css";
-import { useSelector } from "react-redux";
 import TournamentsList from "../components/TournamentsList";
 import commandImage from "../img/NAVI.png";
+import Authorization from "../components/Authorization";
+import {TournamentStore} from "../store/TournamentStore";
 
 function MainPage() {
-  const tournaments = useSelector((state) => state.tournaments);
+
+
   return (
     <main>
       <section className="tier_commands">
@@ -98,40 +100,11 @@ function MainPage() {
           <h1>Актуальные турниры</h1>
         </div>
         <div className="tournamentListMainPage">
-          {tournaments && tournaments.length > 0 && (
-            <TournamentsList tournaments={tournaments} maxDisplay={6} />
-          )}
+
         </div>
       </section>
-      <section className="authorization">
-        <form action="#" method="post" encType="multipart/form-data">
-          <div className="header_auth">
-            <h2>Авторизация</h2>
-          </div>
 
-          <input
-            type="email"
-            name="usermail"
-            placeholder="E-mail или имя пользователя"
-          />
-          <input type="password" name="userpass" placeholder="Пароль" />
-          <div className="remember_auth">
-            <input
-              checked
-              type="checkbox"
-              name="remember"
-              className="remember_user"
-            />
-            <p>Запомнить меня</p>
-          </div>
-          <button>Войти</button>
-          <div className="remember_register">
-            <a href="#">Забыли пароль</a>
-            <div></div>
-            <a href="#">Зарегистрироваться</a>
-          </div>
-        </form>
-      </section>
+     <Authorization/>
     </main>
   );
 }
