2fa82cb1ca9b210ce612b398b7a448be140ef1d2
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-26 12:35:44 +0300

    fix

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,18 +2,19 @@ import { Outlet } from "react-router-dom";
 import React from "react";
 import logo from "../img/Logo.svg";
 import CustomNavLink from "./CustomNavLink";
+import {COMMAND_ROUTE, MAIN_ROUTE, TOURNAMENT_ROUTE} from "../utils/consts";
 function Layout() {
   return (
     <>
       <nav>
         <ul>
-          <CustomNavLink to="/">
+          <CustomNavLink to={MAIN_ROUTE}>
             <img src={logo} alt="" />
           </CustomNavLink>
 
-          <CustomNavLink to="/tournaments">Туринры</CustomNavLink>
+          <CustomNavLink to={TOURNAMENT_ROUTE}>Туринры</CustomNavLink>
 
-          <CustomNavLink to="/commands">Команды</CustomNavLink>
+          <CustomNavLink to={COMMAND_ROUTE}>Команды</CustomNavLink>
 
           <CustomNavLink to="/cabinet">Личный кабинет</CustomNavLink>
         </ul>
