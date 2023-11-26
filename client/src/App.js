f0037b773ddcf46e2cc94d5565bda6c7b6d53d45
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-26 12:09:56 +0300

    add loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,35 @@
-import React, {useEffect} from "react";
-import { Routes, Route } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import CommandsPage from "./pages/CommandsPage";
-import TournamentPage from "./pages/TournamentsPage";
+import React, {useEffect, useState} from "react";
 import Layout from "./components/Layout";
-import CabinetPage from "./pages/CabinetPage";
-import TournamentIn from "./pages/TournamentIn";
-import CommandIn from "./pages/CommandIn";
 import {UserStore} from "./store/UserStore";
 import {check} from "./api/userApi";
+import {AppRouter} from "./components/AppRouter";
+import {getAllGames} from "./api/tournamentApi";
+import {TournamentStore} from "./store/TournamentStore";
+import {Spinner} from "react-bootstrap";
 
 function App() {
   const user = UserStore(state=>state._user)
   const isAuth = UserStore(state=>state._isAuth)
   const setAuth = UserStore(state=>state.setIsAuth)
   const setUser = UserStore(state=>state.setUser)
+  const setGame = TournamentStore(state=>state.setGame)
+  const [loading,setLoading] = useState(true);
   useEffect(() => {
+    getAllGames().then((data)=>setGame(data));
     check().then(data=>{
       if(data){
         setUser(true);
         setAuth(true);}
+    }).finally(()=>setLoading(false));
 
-    })
   }, []);
+  if(loading){
+    return <Spinner animation={"grow"}/>
+  }
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="tournaments/" element={<TournamentPage />} />
-          <Route path="tournaments/:id" element={<TournamentIn />} />
-          <Route path="commands" element={<CommandsPage />} />
-          <Route path="commands/:id" element={<CommandIn />} />
-          <Route path="cabinet" element={<CabinetPage />} />
-        </Route>
-      </Routes>
+      <Layout />
+      <AppRouter/>
     </>
   );
 }
