a942599ea3ceb4c987d2f13885573f9cdb50f9c6
Micklyqq <117474538+Micklyqq@users.noreply.github.com>
2023-11-26 12:35:30 +0300

    get profile info from server

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../css/tournaments.css";
-import ProfileImage from "../img/profile_picture.png";
 import {UserStore} from "../store/UserStore";
+import {jwtDecode} from "jwt-decode";
+import {getData} from "../api/userApi";
+import defaultLogo from "../static/8dd98655-043f-401e-b331-0b4e1bf1f647.png";
+import {Spinner} from "react-bootstrap";
 
-function Profile() {
-    const setAuth = UserStore(state=>state.setIsAuth);
+function Profile({setAuth,isAuth,setUser}) {
+
+
+    const userLogo = UserStore(state=>state._userLogo);
+    const userName = UserStore(state=>state._userName);
+    const setUserName = UserStore(state=>state.setUserName);
+    const setUserLogo = UserStore(state=>state.setUserLogo);
+    const [loading,setLoading]=useState(true);
+
+
     const exit = ()=>{
         setAuth(false);
+        setUser({});
         localStorage.setItem('token','');
     }
+    useEffect(() => {
+        const user = jwtDecode(localStorage.getItem('token'));
+        getData(user.id).then(data => {
+            setUserName(data.userName);
+            setUserLogo(data.logo?(process.env.REACT_APP_API_URL+data.logo):null);
 
+        }).finally(()=>setLoading(false));
+
+    }, [isAuth]);
+    if(loading){
+        return <Spinner animation={"grow"}/>
+    }
   return (
     <section className="profile">
       <div className="profile_header">
@@ -17,10 +40,9 @@ function Profile() {
       </div>
       <div className="profile_info">
         <div className="profile_picture">
-          <img src={ProfileImage} alt="" />
-          <a href="#">Изменить</a>
+          <img src={userLogo?userLogo:defaultLogo} alt="" />
         </div>
-        <p className="nickname">Gamer334</p>
+        <p className="nickname">{userName?userName:"defaulName"}</p>
         <p className="username_info">Команда:OG</p>
         <p className="username_info">Rating: 3400</p>
       </div>
